4411a6b74855bfdf32bff96882b0be3a1d0609a1
Saul Tellez <linux05@Saul2.localdomain>
2025-12-11 21:39:26 -0600

    comentario corregido

--- backend/models.py
+++ backend/models.py
@@ -11,4 +11,4 @@ class Todo(Base):
     title: Mapped[str] = mapped_column(String(255), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
-#archivo 
+#archivo models.py
